0d6fd1f2a6396532700f16d1702cd4138f2cb4ce
Nikita Luchko <nikita.luchko@toptal.com>
2017-01-26 09:38:30 +0400

    Extract `init` function

--- src/index.js
+++ src/index.js
@@ -1,27 +1,9 @@
 // XXX: Main entry point
 // Exposes public methods of the application
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Testshot, {scenario} from 'src/Testshot'
+import init from 'src/init'
+import {scenario} from 'src/Testshot'
 
 module.exports = {
-  // TODO: Extract this function.
-  init: () => {
-    const wrapperElement = document.createElement('div')
-    document.body.appendChild(wrapperElement)
-
-    // TODO: Read from the config, set defaults as well.
-    const options = {
-      server: {
-        host: 'localhost',
-        port: '3001'
-      }
-    }
-
-    ReactDOM.render(
-      React.createElement(Testshot, options),
-      wrapperElement
-    )
-  },
+  init,
   scenario
 }
