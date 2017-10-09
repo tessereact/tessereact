6a2d05e881689065b7d872900f0ec77f9ec91ffb
Lesha Koss <regiusprod@gmail.com>
2017-10-09 16:55:15 +0600

    2x button

--- src/components/ScenarioContent/index.jsx
+++ src/components/ScenarioContent/index.jsx
@@ -29,33 +29,51 @@ class ScenarioContent extends React.Component {
   constructor (props, context) {
     super(props, context)
     this.state = {
-      resizing: false
+      resizing: false,
+      twoColumns: false
     }
   }
 
   render () {
     const {scenario} = this.props
-    return scenario.hasDiff
-      ? this._renderScenarioWithDiff(scenario)
-      : this._renderScenarioWithoutDiff(scenario)
+    return <StyledContent.Wrapper>
+      {this._renderScenarioHeader(scenario)}
+      {this._renderScenarioContent(scenario)}
+    </StyledContent.Wrapper>
   }
 
-  _renderScenarioWithDiff (scenario) {
+  _renderScenarioHeader (scenario) {
+    const {name, context, hasDiff} = scenario
     const {onAcceptSnapshot} = this.props
 
-    return <StyledContent.Wrapper>
-      <Header>
-        <div>
-          <span>{scenario.name}</span>
-        </div>
-        <div>
-          <a href={`/contexts/${scenario.context}/scenarios/${scenario.name}/view`} target='_blank'>
-            <Button>View</Button>
-          </a>
-          {scenario.hasDiff && <AcceptButton onClick={onAcceptSnapshot}>Accept & next</AcceptButton>}
-        </div>
-      </Header>
-      <PanelGroup
+    return <Header>
+      <div>
+        <span>{name}</span>
+      </div>
+      <div>
+        <Button
+          selected={this.state.twoColumns}
+          onClick={() => this.setState({twoColumns: !this.state.twoColumns})}
+        >
+          2x
+        </Button>
+        <a href={`/contexts/${context}/scenarios/${name}/view`} target='_blank'>
+          <Button>View</Button>
+        </a>
+        {hasDiff && <AcceptButton onClick={onAcceptSnapshot}>Accept & next</AcceptButton>}
+      </div>
+    </Header>
+  }
+
+  _renderScenarioContent (scenario) {
+    if (!scenario.hasDiff) {
+      return <ComponentPreview>
+        {this._renderContent(scenario, 'component')}
+      </ComponentPreview>
+    }
+
+    if (this.state.twoColumns) {
+      return <PanelGroup
         onStartResizing={() => this.setState({resizing: true})}
         onStopResizing={() => this.setState({resizing: false})}
         panelWidths={[
@@ -76,31 +94,14 @@ class ScenarioContent extends React.Component {
           </ComponentPreview>
         </ComponentPreview.RightPane>
       </PanelGroup>
-    </StyledContent.Wrapper>
-  }
+    }
 
-  _renderScenarioWithoutDiff (scenario) {
-    const {onAcceptSnapshot} = this.props
-
-    const {name, context, hasDiff, diff, diffCSS, screenshotData} = scenario
-
-    return <StyledContent.Wrapper>
-      <Header>
-        <div>
-          <span>{name}</span>
-        </div>
-        <div>
-          <a href={`/contexts/${context}/scenarios/${name}/view`} target='_blank'>
-            <Button>View</Button>
-          </a>
-          {hasDiff && <AcceptButton onClick={onAcceptSnapshot}>Accept & next</AcceptButton>}
-        </div>
-      </Header>
-
-      <ComponentPreview>
-        {this._renderContent(scenario, 'component')}
-      </ComponentPreview>
-    </StyledContent.Wrapper>
+    return <ComponentPreview>
+      {this._renderContent(scenario, 'component')}
+      {this._renderContent(scenario, 'screenshot')}
+      {this._renderContent(scenario, 'html')}
+      {this._renderContent(scenario, 'css')}
+    </ComponentPreview>
   }
 
   /**
@@ -123,7 +124,7 @@ class ScenarioContent extends React.Component {
       case 'resizingComponent':
       case 'component':
       default:
-        return <div className='component-iframe_container'>
+        return <div className={this.state.twoColumns ? 'component-iframe_container' : 'single_column-iframe_container'}>
           {tab === 'resizingComponent' && <div className='component-iframe_overlay' />}
           <ScenarioFrame className='component-iframe' context={scenario.context} name={scenario.name} />
         </div>
